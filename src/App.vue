8e9d1be0cdd7801f2f2f6e56048c7501dc9c519c
DavideCiammetti <davideciammetti97@gmail.com>
2024-02-15 17:50:38 +0100

    feat: aggiunta header cambio titolo pagina e aggiunta style

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,15 @@
 <script>
+    import AppHeader from './components/AppHeader.vue';
+    export default{
+      name:'App',
+        components:{
+            AppHeader,
+        }
+    }
 </script>
 
 <template>
+  <AppHeader></AppHeader>
 </template>
 
 <style>
--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,7 +1,29 @@
 <script>
+    export default{
+        name: 'Header',
+    };
 </script>
 
 <template>
+    <header class="bg-primary py-3">
+        <nav class="d-flex justify-content-around">
+            <p>
+                <a class="text-white" href="#">page</a>
+            </p>
+            <p>
+                <a class="text-white" href="#">page</a>
+            </p>
+            <p>
+                <a class="text-white" href="#">page</a>
+            </p>
+            <p>
+                <a class="text-white" href="#">page</a>
+            </p>
+            <p>
+                <a class="text-white" href="#">page</a>
+            </p>
+        </nav>
+    </header>
 </template>
 
 <style>
