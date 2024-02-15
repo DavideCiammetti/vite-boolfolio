f3ffc60a2aa87438f791d934cea1af68107b6de9
DavideCiammetti <davideciammetti97@gmail.com>
2024-02-15 22:05:22 +0100

    fix: aggiunta cards per i data

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -11,7 +11,7 @@ import { store } from '../store';
         methods:{
             getProjects(){
                 axios.get(this.store.baseUrl + this.store.apiUrls.projects).then((response)=>{
-                    this.store.projects_list = response.data.data;
+                    this.store.projects_list = response.data.data.data;
                     console.log(response);
                 }).catch((error)=>{
                     console.log(error);
@@ -26,8 +26,14 @@ import { store } from '../store';
 
 <template>
     <main>
-        <div v-for=" projects in this.store.projects_list">
-            {{ projects.title }}
+        <div class="d-flex flex-wrap flex-direction-column justify-content-center gap-4 ">
+            <div class="card" v-for=" projects in this.store.projects_list" style="width: 18rem;">
+            <!-- <img :src="{{ projects.img }}" class="card-img-top" alt="..."> -->
+            <div class="card-body">
+                <h5 class="card-title">{{ projects.title }}</h5>
+                <p class="card-text">{{ projects.title }}</p>
+            </div>
+        </div>
         </div>
     </main>
 </template>
