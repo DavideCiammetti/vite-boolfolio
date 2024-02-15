7b4b3fab8660f17314a736e411c4186051742a49
DavideCiammetti <davideciammetti97@gmail.com>
2024-02-15 17:34:04 +0100

    fix: scaffolding + scss + bootstrap + font awesome

---
+++ src/main.js
@@ -0,0 +1,8 @@
+import { createApp } from 'vue'
+//import scss
+import './scss/style.scss'
+// Import Bootstrap's JS
+import * as bootstrap from 'bootstrap'
+import App from './App.vue'
+
+createApp(App).mount('#app')
