314bd98ce54e7d18725fc6bb6e34f91908f11c61
DavideCiammetti <davideciammetti97@gmail.com>
2024-02-15 17:58:41 +0100

    feat: inclusione footer e main in app.vue

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,7 +1,15 @@
 <script>
+    export default{
+        name: 'Footer',
+    }
 </script>
 
 <template>
+    <footer class="bg-secondary py-3">
+        <div>
+            menu
+        </div>
+    </footer>
 </template>
 
 <style>
--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,7 +1,13 @@
 <script>
+    export default{
+        name: 'Main',
+    }
 </script>
 
 <template>
+    <main>
+        sono main
+    </main>
 </template>
 
 <style>
