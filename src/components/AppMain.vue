1f3071799aa1aeea282219bb226a2480fe2761d9
DavideCiammetti <davideciammetti97@gmail.com>
2024-02-15 19:02:02 +0100

    fix: aggiunta di uno store.js per centralizzare i dati e chiamata axios in  main per prendere i dati dal backend

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,6 +1,25 @@
 <script>
+import axios from 'axios';
+import { store } from '../store';
     export default{
         name: 'Main',
+        data(){
+            return{
+                store,
+            }
+        },
+        methods:{
+            getProjects(){
+                axios.get(this.store.baseUrl + this.store.apiUrls.projects).then((response)=>{
+                    console.log(response);
+                }).catch((error)=>{
+                    console.log(error);
+                });
+            },
+        },
+        created(){
+            this.getProjects();
+        }
     }
 </script>
 
