5ff081baa3b6d658d415d93fedf5b1312e0c9c35
DavideCiammetti <davideciammetti97@gmail.com>
2024-02-16 18:01:46 +0100

    fix: aggiunta spinner caricamento e paginazione

--- src/pages/AppProjectsList.vue
+++ src/pages/AppProjectsList.vue
@@ -6,17 +6,34 @@
         data(){
                 return{
                     store,
+                    // variabile per la gestione della paginazione
+                    currClick: 1,
+                    loader: false,
                 }
             },
             methods:{
                 getProjects(){
-                    axios.get(this.store.baseUrl + this.store.apiUrls.projects).then((response)=>{
-                        this.store.projects_list = response.data.data.data;
+                  this.loader = true;
+                    axios.get(this.store.baseUrl + this.store.apiUrls.projects + this.store.pagination + this.currClick).then((response)=>{
+                        this.store.projectsData = response.data;
                         console.log(response);
                     }).catch((error)=>{
                         console.log(error);
+                    }).finally(()=>{
+                      this.loader = false;
                     });
                 },
+                // cambia numero di pagina 
+                nextPage(){
+                  this.currClick++;
+                  this.getProjects();
+                  console.log(this.currClick);
+                },
+                prevPage(){
+                  this.currClick--;
+                  this.getProjects();
+                  console.log(this.currClick);
+                }
             },
             created(){
                 this.getProjects();
@@ -26,16 +43,33 @@
 
 <template>
      <main class="my-5">
-        <div class="d-flex flex-wrap flex-direction-column justify-content-center gap-4 ">
-            <div class="card" v-for=" projects in this.store.projects_list" style="width: 18rem;">
+         <div>
+          <h2 class="ms-4">
+            lista dei projetti
+          </h2>
+          <div class="spinner-border text-primary ms-4" role="status" v-show="this.loader">
+            <span class="visually-hidden">Loading...</span>
+          </div>
+          <div class="d-flex flex-wrap flex-direction-column justify-content-center gap-4 " v-show="!this.loader">
+            <div class="card" v-for=" projects in this.store.projectsData.data?.data" style="width: 18rem;">
             <img :src="this.store.imgUrl + projects.img" class="card-img-top" alt="...">
             <div class="card-body">
                 <h5 class="card-title">{{ projects.title }}</h5>
-                <p class="card-text">{{ projects.title }}</p>
+                <p class="card-text">{{ projects.description }}</p>
                 <p v-for="technology in projects.technologies" >technologies: {{ technology.title}}</p>
             </div>
         </div>
         </div>
+        <div class="d-flex justify-content-between mx-4 mt-4">
+          <div>
+            <button type="button" class="btn btn-primary" @click="prevPage()" v-show="this.store.projectsData.data?.prev_page_url">preview</button>
+          </div>
+
+          <div>
+            <button type="button" class="btn btn-secondary" @click="nextPage()" v-show="this.store.projectsData.data?.next_page_url">next</button>
+          </div>
+        </div>
+         </div>
     </main>
 </template>
 
--- src/store.js
+++ src/store.js
@@ -2,9 +2,13 @@ import {reactive} from 'vue';
 
 export const store = reactive({
     baseUrl:'http://127.0.0.1:8000',
+    // url per prendere dati api json
     apiUrls:{
         projects: '/api/projects',
     },
-    projects_list:[],
+    // oggetto vuoto per prendere tutti i dati  
+    projectsData:{},
     imgUrl: 'http://127.0.0.1:8000/storage/',
+    // parte url per paginazione
+    pagination: '?page=',
 });
